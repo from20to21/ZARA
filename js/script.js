562a4217da5fbb9f614fbda3354cdfc3eb3c6918
lsh58 <tmdgusdl58@gmail.com>
2020-04-08 13:05:50 +0900

    .

--- js/script.js
+++ js/script.js
@@ -5,14 +5,7 @@ $(function () {
         top: "40%"
     }, 2000);
 
-    $('.header__search button').click(search_on);
 
-    function search_on() {
-        $('.header__search input').animate({
-            width: "50%",
-            opacity: "1"
-        }, 1000);
-    }
 
 });
 

ccf07da9ea3000cc44439f33d0d0d5ba3800a283
lsh58 <tmdgusdl58@gmail.com>
2020-04-08 10:21:23 +0900

    .

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 
 $(function () {
-    $('.header__burger').click(function () {
+    $('.nav__wrapper').fadeOut();
 
-    })
+    $('.header__burger').click(menu_fade);
+    $('.nav__exit').click(menu_fade);
 
+    function menu_fade() {
+        $('.nav__wrapper').fadeToggle(800);
+    }
 
 });
+

d2685a3b27e2a063870447ee4687190ca47a6a1a
lsh58 <tmdgusdl58@gmail.com>
2020-04-09 17:10:37 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -29,7 +29,7 @@ $(function () {
         positionVisual = firstScroll * 0.5;
         positionText = firstScroll * 0.7;
         positionNew = firstScroll * (-0.1);
-        positionCollection = firstScroll * -0.2 + 170;
+        positionCollection = firstScroll * -0.2 + 200;
         positionBest = firstScroll * -0.2 + 100;
 
         if (firstScroll > lastScroll) {

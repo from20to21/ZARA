374c353ae3ac7e6e64266a3acaae0244bffb281b
lsh58 <tmdgusdl58@gmail.com>
2020-04-13 15:26:31 +0900

    ,

--- js/sub01.js
+++ js/sub01.js
@@ -4,19 +4,38 @@ $(function () {
     $('.category').click(change);
 
     function change() {
-        if ($(this).hasClass('selected')) {
-            $('.category').removeClass('change');
-            $('.category').removeClass('selected');
-        }
-        else {
-            $('.category').addClass('change');
-            $(this).toggleClass('change');
-            $(this).addClass('selected');
-        }
+
+        $('.category').addClass('change');
+        $(this).toggleClass('change');
+        $(this).addClass('selected');
+
+        $('header').animate({
+            top: "-45px"
+        }, 300)
+        $('.back').animate({
+            opacity: "1",
+            visibility: "visible"
+        }, 300)
         // var category_offset = $(this).offset().top - 50;
         // $('html,body').animate({
         //     scrollTop: category_offset
         // }, 500)
+    }
+
+    $('.back').click(back);
+
+    function back() {
+        $(this).hasClass('selected');
+        $('.category').removeClass('change');
+        $('.category').removeClass('selected');
+
+        $('header').animate({
+            top: "0"
+        }, 300)
+        $('.back').animate({
+            opacity: "0",
+            visibility: "hidden"
+        }, 300)
     }
 });
 

783acfb2a03d6f00096c9ba5b4c8c4347c5a9667
lsh58 <tmdgusdl58@gmail.com>
2020-04-13 09:58:28 +0900

    .

--- js/sub01.js
+++ js/sub01.js
@@ -1,6 +1,16 @@
 
 $(function () {
 
+    $('.category').click(category_slide);
 
+    function category_slide() {
+        if ($(this).next().hasClass('pop')) {
+            $(this).next().toggleClass('pop');
+        }
+        else {
+            $('.category').next().removeClass('pop');
+            $(this).next().toggleClass('pop');
+        }
+    }
 });
 

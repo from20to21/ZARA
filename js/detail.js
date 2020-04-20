0fe3a212bf034614f97a18703214b052ee2c2452
lsh58 <lsh58@gmail.com>
2020-04-20 21:45:17 +0900

    .

--- js/detail.js
+++ js/detail.js
@@ -1,19 +1,19 @@
 $(function () {
 
-    $('.visual__img01').css({
+    $('.visual__img__previous').css({
         backgroundImage: "url(../img/detail/detail002.jpg)"
     });
-    $('.visual__img02').css({
+    $('.visual__img__main').css({
         backgroundImage: "url(../img/detail/detail001.jpg)"
     });
-    $('.visual__img03').css({
+    $('.visual__img__next').css({
         backgroundImage: "url(../img/detail/detail003.jpg)"
     });
     var idx = $('.visual__image').length;
     var currentIdx = parseInt(idx / 2) + 1;
     var lastLeft = 0;
     var presentLeft = parseInt($('.visual__wrapper').css("left"));
-    console.log(presentLeft);
+    // console.log(presentLeft);
     function drag() {//drag item line
         var firstDrag;
         $('.visual__wrapper').draggable({
@@ -31,8 +31,9 @@ $(function () {
         var lastDrag = e.pageX; //remember where drag end (for checking where to move)
         var moveLeft = lastLeft + presentLeft;
         var moveRight = lastLeft - presentLeft;
-        var currentDiv = '.visual__img0' + currentIdx;
-        console.log($this.find(currentDiv).css("background-image"));
+        var nextSrc, previousSrc;
+        // var currentDiv = '.visual__img0' + currentIdx;
+        // console.log($this.find(currentDiv).css("background-image"));
         if (firstDrag > lastDrag) { //if you move to left
             if (currentIdx == idx) {
                 $this.animate({
@@ -45,6 +46,10 @@ $(function () {
                 }, 300);
                 currentIdx++;
             }
+            nextSrc = $('.visual__img__next').css("background-image");
+            $('.visual__img__main').css({
+                "backgroundImage" : nextSrc
+            });
         }
         else { // if you move right
             if (currentIdx == 1) {
@@ -58,9 +63,13 @@ $(function () {
                 }, 300);
                 currentIdx--;
             }
+            previousSrc = $('.visual__img__previous').css("background-image");
+            
+            $('.visual__img__main').css({
+                "backgroundImage" : previousSrc
+            });
         }
-        var currentDiv2 = '.visual__img0' + currentIdx;
-        console.log($this.find(currentDiv2).css("background-image"));
+        // var currentDiv2 = '.visual__img0' + currentIdx;
+        // console.log($this.find(currentDiv2).css("background-image"));
     }
-
 });

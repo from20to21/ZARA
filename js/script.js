7f71751892d1586fa8217f5ffea57069e8d1dca3
lsh58 <tmdgusdl58@gmail.com>
2020-04-21 18:36:17 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -301,9 +301,9 @@ $(function () {
 
     }
 
-    drag1();
-    drag2();
-    drag3();
+    // drag1();
+    // drag2();
+    // drag3();
 
     function parallax() { // to move image as likely parallax
         firstScroll = window.scrollY;

0a8840384ea8d72d5f10608af65f0cd818115836
lsh58 <tmdgusdl58@gmail.com>
2020-04-22 16:20:49 +0900

    ,

--- js/account.js
+++ js/account.js
@@ -1,3 +1,10 @@
 $(function () {
 
+    $('.sign__check button').click(check);
+
+    function check() {
+        $(this).toggleClass("checked");
+        $(this).find('img').toggleClass("checked");
+    }
+
 });

a1059ec6feb5baa7718d3e6c9153383e0d3e1fd2
lsh58 <tmdgusdl58@gmail.com>
2020-04-22 15:21:31 +0900

    .

--- js/account.js
+++ js/account.js
@@ -1,12 +1,3 @@
 $(function () {
-    $('.sign__email input').click(typing);
-    $('.sign__password input').click(typing);
-
-    function typing() {
-        $(this).parent().find('p').animate({
-            top: "-10px",
-            fontSize: "0.7rem"
-        }, 300);
-    }
 
 });
